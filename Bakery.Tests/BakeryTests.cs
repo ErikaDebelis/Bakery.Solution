3778abef0b2bfc232ba3ed9929907a32c975a775
Erika Debelis <erika.debelis@gmail.com>
2021-07-18 19:50:48 -0700

    1st test run- passed- shows correct # of loaves ordered

--- Bakery.Tests/BakeryTests.cs
+++ Bakery.Tests/BakeryTests.cs
@@ -8,70 +8,54 @@ namespace PierresBakery.Tests
   public class BreadTests
   {
     [TestMethod]
-    public void BreadConstructor_InitializesBread_Bread()
-    {
-      // Breads newBread = new Breads();
-      // Assert.AreEqual(typeof(breads), newBread.GetType());
-    }
-
-    [TestMethod]
     public void GetNumberOfLoavesOrdered_ReturnTheCorrectAmountOfLoaves()
     {
       //Arrange
-      var bread = new Bread(); // Model
-      var bakery = new Bakery(); // Class with our functions
+      Bread newBread = new Bread(); // Model
+      Bakery newBakery = new Bakery(); // Class with our functions
       
-      var expectedOutput = 4;
-      var input = "4";
+      int expectedOutput = 4;
+      string input = "4";
       
       //Act
-      int ouput = bakery.GetNumberOfLoavesOrdered(input);
+      int output = newBakery.GetNumberOfLoavesOrdered(input);
 
       //Assert
-      Assert.AreEqual(expectedOutput, ouput);
+      Assert.AreEqual(expectedOutput, output);
 
     }
 
-    [TestMethod]
-    public void GetCostOfOneLoaf_InitializesBread_Bread()
-    {
-      //Arrange
+//     [TestMethod]
+//     public void GetCostOfOneLoaf_InitializesBread_Bread()
+//     {
+//       //Arrange
 
-      //Act
+//       //Act
 
-      //Assert
-    }
+//       //Assert
+//     }
 
-    [TestMethod]
-    public void SetTotalBreadCartCost_InitializesBread_Bread()
-    {
-      //Arrange
+//     [TestMethod]
+//     public void SetTotalBreadCartCost_InitializesBread_Bread()
+//     {
+//       //Arrange
 
-      //Act
+//       //Act
 
-      //Assert
-    }
+//       //Assert
+//     }
 
-    [TestMethod]
-    public void SetBreadDeal_InitializesBread_Bread()
-    {
-      //Arrange
+//     [TestMethod]
+//     public void SetBreadDeal_InitializesBread_Bread()
+//     {
+//       //Arrange
 
-      //Act
+//       //Act
 
-      //Assert
-    }
+//       //Assert
+//     }
 
 
   }
-//     [TestClass]
-//   public class PastryTests
-//   {
-//     [TestMethod]
-//     public void PastryConstructor_InitializesPastry_Bread()
-//     {
-//       Pastries newPastry = new Pastries();
-//       Assert.AreEqual(typeof(Pastries), newPastry.GetType());
-//     }
-//   }
+
 }
