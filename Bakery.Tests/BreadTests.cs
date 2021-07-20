27c393da11a0149f949e882a2d5921211c558bce
Erika Debelis <erika.debelis@gmail.com>
2021-07-19 20:53:19 -0700

    delete bakery.cs + bakerytests.cs

--- Bakery.Tests/BreadTests.cs
+++ Bakery.Tests/BreadTests.cs
@@ -8,22 +8,7 @@ namespace PierresBakery.Tests
     [TestClass]
   public class BreadTests
   {
-    [TestMethod]
-    public void GetNumberOfLoavesOrdered_ReturnTheCorrectAmountOfLoaves()
-    {
-      //Arrange
-      Bread newBread = new Bread(); // Model
 
-      int expectedOutput = 4;
-      string input = "4";
-
-      //Act
-      int output = newBread.GetNumberOfLoavesOrdered(input);
-
-      //Assert
-      Assert.AreEqual(expectedOutput, output);
-
-    }
     [TestMethod]
     public void SetBreadTotal_SetValuesToPropertiesOfBread_Bread()
     {
